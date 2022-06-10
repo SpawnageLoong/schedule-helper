1e9ddac6b1d87516fcbe2e87ff8595faaa7151ba
SpawnageLoong <10321583+SpawnageLoong@users.noreply.github.com>
2022-06-10 14:10:39 +0800

    copied workshop todo list

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import { useState, useEffect } from 'react'
 import { supabase } from './Components/supabaseClient'
 import Auth from './Components/Auth'
 import Account from './Components/Account'
+import Render from './Components/Render'
+import Todo from './Components/Todo'
 
 function App() {
   const [session, setSession] = useState(null)
+  const [tasks, setTasksState] = useState([])
 
   useEffect(() => {
     setSession(supabase.auth.session())
@@ -15,9 +18,26 @@ function App() {
     })
   }, [])
 
+  function setTasks(newTasks) {
+    setTasksState(newTasks);
+    // localStorage can only store strings, so for us to store objects
+    // we must first stringify the object into a JSON string
+    window.localStorage.setItem("tasks", JSON.stringify(newTasks));
+  }
+
+  useEffect(() => {
+    // localStorage can only store strings, so we store our array of tasks
+    // as a JSON string, and decode the JSON string into an array when we read it
+    const savedTasks = JSON.parse(window.localStorage.getItem("tasks"));
+    setTasksState(savedTasks ?? []);
+  }, []);
+
   return (
     <div className="container" style={{ padding: '50px 0 100px 0' }}>
-      {!session ? <Auth /> : <Account key={session.user.id} session={session} />}
+      {/* {!session ? <Auth /> : <Render />}
+      This line has been replaced due to inactive supabase project.
+      Reuse when reactivating Supabase project.*/}
+      {<Todo tasks={tasks} setTasks={setTasks} />}
     </div>
   )
 }
---
+++ src/Components/Render.js
@@ -0,0 +1,6 @@
+import Account from './Account'
+import Todo from './Todo'
+
+export default function render() {
+    return <Todo />
+}
